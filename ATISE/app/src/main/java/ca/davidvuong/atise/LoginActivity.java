656de6469a7760b233b4bcaaa45f0390173526a0
lloydtorres <lloyd.torres95@gmail.com>
2015-09-19 12:20:01 -0400

    Normalizing project

---
+++ ATISE/app/src/main/java/ca/davidvuong/atise/LoginActivity.java
@@ -0,0 +1,22 @@
+package ca.davidvuong.atise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lloyd on 2015-09-19.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
