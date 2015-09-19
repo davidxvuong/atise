bb1852e3d7fb243ab1f9dd234801ed8d87d913da
lloydtorres <lloyd.torres95@gmail.com>
2015-09-19 17:45:47 -0400

    Finish login screen and auth

--- ATISE/app/src/main/java/ca/davidvuong/atise/LoginActivity.java
+++ ATISE/app/src/main/java/ca/davidvuong/atise/LoginActivity.java
@@ -3,20 +3,55 @@ package ca.davidvuong.atise;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 /**
  * Created by Lloyd on 2015-09-19.
  */
 public class LoginActivity extends AppCompatActivity {
+    Firebase ref;
+    EditText username;
+    EditText password;
+    Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        Firebase.setAndroidContext(this);
+        ref = new Firebase("https://luminous-fire-9033.firebaseio.com");
+
+        username = (EditText) findViewById(R.id.main_username);
+        password = (EditText) findViewById(R.id.main_password);
+        loginBtn = (Button) findViewById(R.id.main_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.authWithPassword(username.getText().toString(), password.getText().toString(), new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        Intent launchApp = new Intent(LoginActivity.this, ShoppingActivity.class);
+                        startActivity(launchApp);
+                        finish();
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        Toast.makeText(getApplicationContext(), "Login failed. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
