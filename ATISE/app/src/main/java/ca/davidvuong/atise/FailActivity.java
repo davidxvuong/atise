7f9caedba92332678c9832eee658a821944997e8
David Vuong <david.vuong@uwaterloo.ca>
2015-09-20 04:12:01 -0400

    Continue button listeners implemented

--- ATISE/app/src/main/java/ca/davidvuong/atise/FailActivity.java
+++ ATISE/app/src/main/java/ca/davidvuong/atise/FailActivity.java
@@ -3,16 +3,30 @@ package ca.davidvuong.atise;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Lloyd on 2015-09-19.
  */
 public class FailActivity extends AppCompatActivity{
+    Button continueBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fail);
         getSupportActionBar().hide();
+
+        continueBtn = (Button)findViewById(R.id.fail_btn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(FailActivity.this, ShoppingActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     @Override
--- ATISE/app/src/main/java/ca/davidvuong/atise/SuccessActivity.java
+++ ATISE/app/src/main/java/ca/davidvuong/atise/SuccessActivity.java
@@ -3,16 +3,30 @@ package ca.davidvuong.atise;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Lloyd on 2015-09-19.
  */
 public class SuccessActivity extends AppCompatActivity {
+    Button continueBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
         getSupportActionBar().hide();
+
+        continueBtn = (Button)findViewById(R.id.success_btn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SuccessActivity.this, ShoppingActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     @Override
